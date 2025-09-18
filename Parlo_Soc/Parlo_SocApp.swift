b41a74cc6893e0410fee81fe2e08d2394fa5a972
Michelle <michellegurovith053@gmail.com>
2025-09-17 20:04:00 -0700

    Initial Commit

---
+++ Parlo_Soc/Parlo_SocApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parlo_SocApp.swift
+//  Parlo_Soc
+//
+//  Created by Michelle Gurovith on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Parlo_SocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Parlo_SocTests/Parlo_SocTests.swift
@@ -0,0 +1,17 @@
+//
+//  Parlo_SocTests.swift
+//  Parlo_SocTests
+//
+//  Created by Michelle Gurovith on 9/17/25.
+//
+
+import Testing
+@testable import Parlo_Soc
+
+struct Parlo_SocTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
